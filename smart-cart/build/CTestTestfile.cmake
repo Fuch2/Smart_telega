5374070a390e61d7413435ae11972318dbe83e50
Fuch2 <aynur.galiullin.03@gmail.com>
2026-03-31 03:03:32 +0300

    Remove build artifacts and ignore build directory

--- smart-cart/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/fuch/Document/Smart_Telega/smart-cart
-# Build directory: /Users/fuch/Document/Smart_Telega/smart-cart/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/fuch/Document/Smart_Telega/smart-cart/build/frame_codec_tests[1]_include.cmake")
-include("/Users/fuch/Document/Smart_Telega/smart-cart/build/integration_tests[1]_include.cmake")
-subdirs("build_qt")
--- smart-cart/build/build_qt/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/fuch/Document/Smart_Telega/smart-cart/src/presentation/qt
-# Build directory: /Users/fuch/Document/Smart_Telega/smart-cart/build/build_qt
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
